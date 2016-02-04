2426598eb5f839f0b44f47a7e524904f7fd39d53
Colin Tucker <colin@praxis.net.au>
2016-02-04 15:22:32 +1100

    Upgraded to font-awesome 4.5.0
    Now using YAML config API for icon/unicode mappings, plus added
    font-awesome CSS to LeftAndMain extra requirements (font-awesome is now
    able to be blocked by the Requirements class if necessary). Also cleaned
    up file line endings and formatting.

---
+++ _config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Define Module Directory:
+
+if (!defined('MODERNO_ADMIN_DIR')) {
+    define('MODERNO_ADMIN_DIR', basename(__DIR__));
+}
--- javascript/moderno.js
+++ javascript/moderno.js
@@ -1,42 +1,42 @@
 (function($) {
-	
-	// Define Variables:
-	
-	var selector   = "#cms-content-tools-CMSMain";
-	var cookieName = "cms-content-tools-width";
-	
-	// Make Menu Resizable:
-	
-	$(selector).entwine({
-		
-		onadd: function() {
-			
-			var self = this;
-			
-			self.resizable({
-				
-				handles: 'e',
-				
-				resize: function(event,ui) { $.cookie(cookieName, self.outerWidth()); }
-				
-			});
-			
-		}
-		
-	});
-	
-	// Resize Menu after Ajax Load:
-	
-	$(document).ajaxComplete(function() {
-		
-		if (!$(selector).hasClass('collapsed')) {
-			
-			var width = $.cookie(cookieName);
-			
-			if (width != null) $(selector).css('width', width + 'px');
-			
-		}
-		
-	});
-	
+    
+    // Define Variables:
+    
+    var selector   = "#cms-content-tools-CMSMain";
+    var cookieName = "cms-content-tools-width";
+    
+    // Make Menu Resizable:
+    
+    $(selector).entwine({
+        
+        onadd: function() {
+            
+            var self = this;
+            
+            self.resizable({
+                
+                handles: 'e',
+                
+                resize: function(event,ui) { $.cookie(cookieName, self.outerWidth()); }
+                
+            });
+            
+        }
+        
+    });
+    
+    // Resize Menu after Ajax Load:
+    
+    $(document).ajaxComplete(function() {
+        
+        if (!$(selector).hasClass('collapsed')) {
+            
+            var width = $.cookie(cookieName);
+            
+            if (width != null) $(selector).css('width', width + 'px');
+            
+        }
+        
+    });
+    
 })(jQuery);
